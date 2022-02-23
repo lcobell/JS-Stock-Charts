a06bb826539e0ff99d5d00a941d3509483b5e263
lcobell <lcobell@gmail.com>
2022-02-23 12:37:47 -0600

    complete to chart one
    Chart one is working using the mock data

--- public/index.js
+++ public/index.js
@@ -1,16 +1,54 @@
 async function main() {
-    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=9822bc9d38104c46b9245fb77447a12f')
-    let responseText = await response.json();
+    // let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=9822bc9d38104c46b9245fb77447a12f')
+    // let responseText = await response.json();
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
     
-    // const { GME, MSFT, DIS, BNTX } = result;
+    // What to do now....?
 
-    // const stocks = [GME, MSFT, DIS, BNTX];
+    const { GME, MSFT, DIS, BNTX } = mockData;
+    const stocks = [GME, MSFT, DIS, BNTX];
+
     
-    console.log(responseText)
+    console.log(stocks[0].values)                                                
+    stocks[0].values.map( value => value. _)
+    
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value =>value.datetime),
+            datasets: stocks.map( stock => ( {
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+       
+
+        
+        }
+    });
+    console.log(stocks[0].values)                                                
+   console.log(mockData)
+    
+    // console.log(responseText)
 }
 
 main()
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
